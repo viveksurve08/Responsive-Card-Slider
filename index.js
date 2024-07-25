402ba5078d281ae44190e0441d5d4c2a9995846e
VivEk SuRve <108250212+viveksurve08@users.noreply.github.com>
2024-07-26 00:58:38 +0530

    responsive card slider

---
+++ index.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
+  });
+  
